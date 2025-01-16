c71c6d717fc87f55e2c0749b2bc31443b2ae7e16
LEE <lecongduy70@gmail.com>
2025-01-16 12:35:03 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// JavaScript cho việc hiển thị/ẩn thanh tìm kiếm khi nhấn vào biểu tượng kính lúp
+document.getElementById("search-icon").addEventListener("click", function() {
+    var searchContainer = document.getElementById("search-container");
+    if (searchContainer.style.display === "none" || searchContainer.style.display === "") {
+        searchContainer.style.display = "block"; // Hiển thị thanh tìm kiếm
+    } else {
+        searchContainer.style.display = "none"; // Ẩn thanh tìm kiếm
+    }
+});
+
+                            //SWIPER BILLBOARD
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper(".main-swiper", {
+        slidesPerView: 3,  // Số lượng hình ảnh hiển thị trong mỗi lần
+        spaceBetween: 70,  // Khoảng cách giữa các hình
+        loop: true,
+        navigation: {
+            nextEl: ".icon-arrow-right",
+            prevEl: ".icon-arrow-left",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 1,  // Màn hình nhỏ, chỉ 1 hình
+            },
+            768: {
+                slidesPerView: 2,  // Màn hình vừa, hiển thị 2 hình
+            },
+            1024: {
+                slidesPerView: 3,  // Màn hình lớn, hiển thị 3 hình
+            },
+        },
+    });
+});
+
+  
