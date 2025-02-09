a1e1c5c086c9b7e91eace35114518ae55871b42e
LEE <lecongduy70@gmail.com>
2025-02-09 16:30:13 +0900

    Them video

---
+++ js/men.js
@@ -0,0 +1,97 @@
+// JavaScript cho việc hiển thị/ẩn thanh tìm kiếm khi nhấn vào biểu tượng kính lúp
+document.getElementById("search-icon").addEventListener("click", function() {
+    var searchContainer = document.getElementById("search-container");
+    if (searchContainer.style.display === "none" || searchContainer.style.display === "") {
+        searchContainer.style.display = "block"; // Hiển thị thanh tìm kiếm
+    } else {
+        searchContainer.style.display = "none"; // Ẩn thanh tìm kiếm
+    }
+});
+
+// Initialize Swiper
+const swiper = new Swiper(".mySwiper", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
+
+// Initialize Swiper for each collection
+const summerSwiper = new Swiper(".summerSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+const winterSwiper = new Swiper(".winterSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+const accessoriesSwiper = new Swiper(".accessoriesSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
